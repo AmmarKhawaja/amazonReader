4dd2fd0b2cfe77b0d7d401d6e7e33c7bed396fbb
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-06-13 21:21:39 -0400

    Cleaned up

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-if __name__ == "__main__":
-    print("hello")
+import amazonplug
+from privateinfo import *
+from sp_api.api import Orders
+from sp_api.api import Reports
+from sp_api.api import Feeds
+from sp_api.base import SellingApiException
+from sp_api.base.reportTypes import ReportType
+from datetime import datetime, timedelta
 
+
+if __name__ == "__main__":
+
+    amazonplug.generateMerchantReport(7)
