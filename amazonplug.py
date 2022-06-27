e15b6d02131cad1d1e591c11051437c3a409d096
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-06-27 15:22:07 -0400

    Added function to parse xml file and return code-friendly dict

--- amazonplug.py
+++ amazonplug.py
@@ -7,6 +7,21 @@ from sp_api.base import SellingApiException
 from sp_api.base.reportTypes import ReportType
 from datetime import datetime, timedelta
 import time
+import xml.etree.ElementTree as Xet
+import pandas as pd
+import csv
+
+def xmlParse(FILENAME):
+    dic = {}
+    xmlparse = Xet.parse(FILENAME)
+    root = xmlparse.getroot()
+    #child = root.getchildren()
+    for i in root.iter():
+        dic[i.tag] = []
+    for i in root.iter():
+        dic[i.tag].append(i.text)
+    return dic
+
 def printOrders():
     print(Orders(credentials=credentials).get_orders(CreatedAfter=(datetime.utcnow() - timedelta(days=7)).isoformat()))
 
@@ -16,11 +31,15 @@ def generateOrdersReport(DAYS,FILENAME):
         dataStartTime=datetime.utcnow() - timedelta(days=DAYS))
 
     status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
+    print("Generating Report")
     while(status != 'DONE'):
         time.sleep(5)
         status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
-        print("Generating Report")
+        print("...", end=" ")
+
     Reports(credentials=credentials).get_report_document(Reports(credentials=credentials).get_report(res.payload['reportId']).payload['reportDocumentId'], download=True, file=FILENAME)
+    print("Generated")
+    return FILENAME
 
 def generateMerchantReport(DAYS,FILENAME):
     res = Reports(credentials=credentials).create_report(
@@ -34,4 +53,4 @@ def generateMerchantReport(DAYS,FILENAME):
         print("Generating Report")
     Reports(credentials=credentials).get_report_document(
         Reports(credentials=credentials).get_report(res.payload['reportId']).payload['reportDocumentId'], download=True,
-        file=FILENAMEA)
+        file=FILENAME)
