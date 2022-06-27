ea29906aa3b504c50c4e18caa1e40b64436144d6
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-06-27 15:42:26 -0400

    Updated for amazonplug

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 import amazonplug
-from privateinfo import *
-from sp_api.api import Orders
-from sp_api.api import Reports
-from sp_api.api import Feeds
-from sp_api.base import SellingApiException
-from sp_api.base.reportTypes import ReportType
-from datetime import datetime, timedelta
-
 
 if __name__ == "__main__":
-
-    amazonplug.generateMerchantReport(7)
+    a = amazonplug.xmlParse(amazonplug.generateOrdersReport(7, 'report.xml'))
+    print(a)
