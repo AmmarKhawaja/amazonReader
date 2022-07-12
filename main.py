595e6bcbad0bfef60ea5fbc4bf35153939110d1d
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-07-12 12:38:18 -0500

    Updated for changes in amazonplug

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import amazonplug
+import amazonplug as ap
 
 if __name__ == "__main__":
-    a = amazonplug.xmlParse(amazonplug.generateOrdersReport(7, 'report.xml'))
+    a = ap.generateOrdersReport(7, 'report.xml')
     print(a)
