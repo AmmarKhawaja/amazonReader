22b54cdf1a319a3f76d851dfc65d9535e15ec046
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-07-12 18:05:18 -0500

    Added first function to parse data from xml

--- datareader.py
+++ datareader.py
@@ -1,1 +1,16 @@
 #Reads data from amazonplug (CSV)
+def parseOrderMonetary(FILE):
+    title = ' '
+    type = ' '
+    dic = {}
+    for i in FILE.iter():
+        if (i.tag == 'AmazonOrderID'):
+            title = i.text
+            dic[title] = {}
+        elif (i.tag == 'ProductName'):
+            dic[title][i.tag] = i.text
+        elif (i.tag == 'Type'):
+            type = i.text
+        elif (i.tag == 'Amount'):
+            dic[title][type] = i.text
+    return dic
