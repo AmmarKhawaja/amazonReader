d3f66d0f5e99340c12bc72701ceb6c4c8246e5d9
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-07-13 18:17:27 -0500

    Refactored code and added invalid input handling.

--- amazonplug.py
+++ amazonplug.py
@@ -10,40 +10,46 @@ import time
 import xml.etree.ElementTree as Xet
 import pandas as pd
 import csv
+def loadingReport(res):
+    status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
+    # Generator Start
+    print('Generating Report')
+    while (status != 'DONE'):
+        time.sleep(5)
+        status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
+        print("...", end=" ")
+    print('Generated')
+
+def parse(FILENAME):
+    xmlparse = Xet.parse(FILENAME)
+    root = xmlparse.getroot()
+    return root
 
 def printOrders():
     print(Orders(credentials=credentials).get_orders(CreatedAfter=(datetime.utcnow() - timedelta(days=7)).isoformat()))
 
 def generateOrdersReportFromNow(DAYS,FILENAME):
+    if(DAYS > 30):
+        print("Days cannot exceed 30")
+        return;
     res = Reports(credentials=credentials).create_report(
-        reportType=ReportType.GET_XML_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL,
-        dataStartTime=datetime.utcnow() - timedelta(days=DAYS))
-
-    status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
-    #Generator Start
-    print('Generating Report')
-    while(status != 'DONE'):
-        time.sleep(5)
-        status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
-        print("...", end=" ")
-
+        reportType=ReportType.GET_FLAT_FILE_SALES_TAX_DATA,
+        dataStartTime=datetime.utcnow() - timedelta(days=DAYS)
+    )
+    loadingReport(res)
     Reports(credentials=credentials).get_report_document(Reports(credentials=credentials).get_report(res.payload['reportId']).payload['reportDocumentId'], download=True, file=FILENAME)
-    print('Generated')
-    #Generator End
-    xmlparse = Xet.parse(FILENAME)
-    root = xmlparse.getroot()
-    return root
+    return parse(FILENAME)
 
-def generateMerchantReport(DAYS,FILENAME):
-    res = Reports(credentials=credentials).create_report(
-        reportType=ReportType.GET_MERCHANT_LISTINGS_ALL_DATA,
-        dataStartTime=datetime.utcnow() - timedelta(days=DAYS))
-
-    status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
-    while (status != 'DONE'):
-        time.sleep(5)
-        status = Reports(credentials=credentials).get_report(res.payload['reportId']).payload['processingStatus']
-        print("Generating Report")
-    Reports(credentials=credentials).get_report_document(
-        Reports(credentials=credentials).get_report(res.payload['reportId']).payload['reportDocumentId'], download=True,
-        file=FILENAME)
+# Need to enable permissions in seller central.
+# def generateOrdersReportDateRange(Byear, Bmonth, Bday, Eyear, Emonth, Eday, FILENAME):
+#     res = Reports(credentials=credentials).create_report(
+#         reportType=ReportType.GET_XML_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL,
+#         dataStartTime=datetime(Byear, Bmonth, Bday).isoformat(),
+#         dateEndTime=datetime(Eyear, Emonth, Eday).isoformat(),
+#     )
+#
+#     loadingReport(res)
+#     Reports(credentials=credentials).get_report_document(
+#         Reports(credentials=credentials).get_report(res.payload['reportId']).payload['reportDocumentId'], download=True,
+#         file=FILENAME)
+#     return parse(FILENAME)
