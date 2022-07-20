c8a2d9b709f50cfaab5ad1019e78703d1b18a2ef
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-07-19 21:45:23 -0500

    Added getSalesByItem function

--- datareader.py
+++ datareader.py
@@ -16,3 +16,18 @@ def parseOrderMonetary(REPORT):
         elif (i.tag == 'Amount'):
             dic[title][type] = i.text
     return dic
+
+def getSalesByItem(REPORT):
+    dic = parseOrderMonetary(REPORT)
+    sales = {}
+    print(dic)
+    for i in dic:
+        print(i)
+        inst = dic[i]
+        if 'ProductName' in inst and 'Principal' in inst:
+            if inst['ProductName'] in sales:
+                sales[inst['ProductName']] += float(inst['Principal'])
+            else:
+                sales[inst['ProductName']] = 0
+    print(sales)
+
